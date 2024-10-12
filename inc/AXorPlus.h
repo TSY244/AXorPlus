cbd712f0ee319cd7e0b7c6e9bde5e303e656becd
TSY244 <1241407552@qq.com>
2024-10-12 09:58:20 +0800

    init

---
+++ inc/AXorPlus.h
@@ -0,0 +1,18 @@
+//
+// Created by au9u5t on 24-10-12.
+//
+
+#ifndef AXORPLUS_AXORPLUS_H
+#define AXORPLUS_AXORPLUS_H
+
+#include <string>
+
+class AXorPlus {
+private:
+    void printHelp();
+public:
+
+};
+
+
+#endif //AXORPLUS_AXORPLUS_H
---
+++ src/AXorPlus.cpp
@@ -0,0 +1,9 @@
+//
+// Created by au9u5t on 24-10-12.
+//
+
+#include "../inc/AXorPlus.h"
+
+void AXorPlus::printHelp() {
+
+}
